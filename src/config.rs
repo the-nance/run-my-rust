354df88e3db12730c870d65c12d075e77518d285
alligator <guamjust@gmail.com>
2022-03-26 19:33:29 -0400

    update bot add role stuff

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,39 @@
 use serde::{Deserialize, Serialize};
+use once_cell::sync::Lazy;
+use std::{fs::read_to_string};
 
-use std::{fs::read_to_string, lazy::SyncLazy};
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ButtonMenuRole {
+	pub id: u64,
+	pub label: String,
+	pub style: u8
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ButtonMenu {
+	pub channel_id: u64,
+	pub roles: Vec<ButtonMenuRole>,
+	pub message: String,
+	pub comp_type: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ButtonMenuSettings {
+	pub send_on_start: bool,
+	pub messages_to_check: u64
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub token: String,
+	pub bot_id: u64,
     pub channels: Vec<u64>,
     pub banned_roles: Vec<u64>,
+	pub settings: ButtonMenuSettings,
+	pub button_menus: Vec<ButtonMenu>
 }
 
-pub static CONFIG: SyncLazy<Config> = SyncLazy::new(|| {
+pub static CONFIG: Lazy<Config> = Lazy::new(|| {
     let contents = read_to_string("config.toml").expect("Could not open config.toml");
     toml::from_str(&contents).unwrap()
 });
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
-#![feature(once_cell)]
-
-use futures::{future::FutureExt, Stream};
 // use futures_util::{TryStreamExt,StreamExt};
 use futures::StreamExt;
-use futures_util::{future, pin_mut, stream::SplitSink, SinkExt};
+
 use hyper::body::Buf;
-use hyper::{body::HttpBody, client::HttpConnector, Body, Client as HyperClinet, Request};
+use hyper::{client::HttpConnector, Body, Client as HyperClinet, Request};
 use hyper_tls::HttpsConnector;
+use twilight_http::request::AuditLogReason;
 use std::{
     error::Error,
-    io::{Read, Write},
-    sync::{Arc, Mutex},
-    time::Duration,
+    sync::{Arc},
 };
-use tokio::{net::TcpStream, sync::mpsc, task};
+
+use tokio::{task};
 use tracing::{error, info};
 use twilight_cache_inmemory::{InMemoryCache, InMemoryCacheBuilder, ResourceType};
 use twilight_gateway::{cluster::ClusterBuilder, Event, Intents};
 use twilight_http::{request::channel::reaction::RequestReactionType, Client};
 use twilight_model::{
-    guild::{audit_log::AuditLogEventType, Permissions},
     id::{
-        marker::{GuildMarker, RoleMarker, UserMarker},
+        marker::{RoleMarker, ChannelMarker,ApplicationMarker},
         Id,
     },
+	application::{component::{Component,action_row::ActionRow,button::{Button,ButtonStyle}}},
+	http::interaction::{InteractionResponse,InteractionResponseType},
+	channel::message::MessageFlags
 };
 
 mod config;
@@ -40,7 +39,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let (cluster, mut events) = ClusterBuilder::new(
         token.clone(),
-        Intents::GUILD_BANS | Intents::GUILDS | Intents::GUILD_MESSAGES,
+        Intents::GUILD_BANS | Intents::GUILDS | Intents::GUILD_MESSAGES | Intents::GUILD_MEMBERS,
     )
     .build()
     .await?;
@@ -58,19 +57,89 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let http = Arc::new(Client::builder().token(token).build());
     let hyper_cline =
         Arc::new(HyperClinet::builder().build::<_, hyper::Body>(HttpsConnector::new()));
+	
+	let http_clone = http.clone();
+	task::spawn(async move {
+		let http = http_clone.clone();
+
+		if config::CONFIG.settings.send_on_start {
+			for button_menu in &config::CONFIG.button_menus {
+				let channel_id = Id::<ChannelMarker>::new(button_menu.channel_id);
+				let channel_messages = http
+					.channel_messages(channel_id)
+					.limit(config::CONFIG.settings.messages_to_check as u16).unwrap()
+					.exec()
+					.await.unwrap();
+
+				for message in channel_messages.models().await.unwrap() {
+					if message.author.bot && message.author.id.get() == config::CONFIG.bot_id {
+						if message.content == button_menu.message {
+							match http.delete_message(channel_id, message.id).reason("Sending new button menu").unwrap().exec().await {
+								Ok(_) => info!("Should've deleted?"),
+								Err(_) => error!("Failed to delete a bot's message while starting"),
+							}
+						}
+						continue;
+					}
+
+					match http.create_message(channel_id)
+						.content(&button_menu.message).unwrap()
+						.components(&[
+							Component::ActionRow(
+								ActionRow {
+									components: button_menu.roles.iter().map(|r| {
+										let role_label = format!("{}", r.id);
+										Component::Button(Button {
+									        custom_id: Some(role_label),
+									        disabled: false,
+									        emoji: None,
+									        label: Some(r.label.clone()),
+									        style: match r.style {
+												1 => ButtonStyle::Primary,
+												2 => ButtonStyle::Secondary,
+												3 => ButtonStyle::Success,
+												4 => ButtonStyle::Danger,
+												_ => ButtonStyle::Secondary
+											},
+									        url: None,
+									    })
+									}).collect::<Vec<Component>>()
+								})]
+						).unwrap().exec().await {
+					        Ok(_) => {},
+					        Err(_) => error!("Failed to send a button menu."),
+					    }
+					break;
+				}
+				
+			}
+		}
+	});
 
     tokio::spawn(async move {
         cluster.up().await;
     });
 
     while let Some(event) = events.next().await {
+		cache.update(&event.1);
         let cache = cache.clone();
         let http = http.clone();
         let hyper = hyper_cline.clone();
 
         tokio::spawn(async move {
-            if let Err(e) = handle_event(cache, http, event, hyper).await {
-                error!("Error handling event: {}", e);
+            if handle_event(cache, http.clone(), event.clone(), hyper).await.is_err() {
+                error!("Error handling event");
+				match event.1 {
+					Event::MessageCreate(message) => {
+						let _message = http
+							.create_message(message.channel_id)
+							.content(":x: there was an error handling that event. please repooort me.")
+							.unwrap()
+							.exec()
+							.await;
+					}
+					_ => error!("Error from event we dont even handle...")
+				}
             };
         });
     }
@@ -89,6 +158,95 @@ async fn handle_event(
         Event::Ready(_) => {
             info!("Shard {} is now ready", shard_id);
         }
+		Event::InteractionCreate(interaction) => {
+			match &interaction.0 {
+			    twilight_model::application::interaction::Interaction::MessageComponent(msgcmp) => {
+					http.interaction(Id::<ApplicationMarker>::new(config::CONFIG.bot_id))
+						.create_response(
+							msgcmp.id, 
+							&msgcmp.token,
+							&InteractionResponse {
+								kind: InteractionResponseType::DeferredUpdateMessage,
+								data: None,
+				
+					        }
+						)
+						.exec()
+						.await.unwrap();
+
+					let guild_id = match interaction.guild_id() {
+				        Some(g) =>g,
+				        None => {return Ok(())}
+				    };
+					
+					if let Some(member) = &msgcmp.member {
+						if let Some(user) = &member.user {
+							if cache.member(guild_id, user.id).is_none() {
+								http.interaction(Id::<ApplicationMarker>::new(config::CONFIG.bot_id))
+									.create_followup(
+										&msgcmp.token,
+									)
+									.content("You're not cached. Send a message somewhere and press me again.").unwrap()
+									.flags(MessageFlags::EPHEMERAL)
+									.exec()
+									.await.unwrap();
+
+								return Ok(());							
+							}
+
+							let member = cache.member(guild_id, user.id).unwrap();
+	
+							for role in &config::CONFIG.banned_roles {
+								if member.roles().contains(&Id::<RoleMarker>::new(*role)) {
+									info!("Banned user tried running getting a role");
+									return Ok(());
+								}
+							}
+	
+							let role_id = match msgcmp.data.custom_id.parse::<u64>() {
+								Ok(id) => Id::<RoleMarker>::new(id),
+								Err(_) => {
+									http.interaction(Id::<ApplicationMarker>::new(config::CONFIG.bot_id))
+										.create_followup(
+											&msgcmp.token,
+										)
+										.content("Failed to get the role id. Sorry.").unwrap()
+										.flags(MessageFlags::EPHEMERAL)
+										.exec()
+										.await.unwrap();
+
+									return Ok(())
+								}
+							};
+	
+							let mut roles = member.roles().to_vec().clone();
+	
+							let message = if roles.contains(&role_id) {							
+								let new_roles = roles.iter().filter(|e| *e != &role_id).copied().collect::<Vec<Id<RoleMarker>>>();
+								http.update_guild_member(guild_id, user.id)
+									.roles(&new_roles).exec().await.unwrap();
+									format!("<:ferrischeck:957417376314429490> removed <@&{}>", role_id.get())
+							} else {
+								roles.push(role_id);
+								http.update_guild_member(guild_id, user.id)
+									.roles(&roles).exec().await.unwrap();
+								format!("<:ferrischeck:957417376314429490> added <@&{}>", role_id.get())
+							};
+							
+							http.interaction(Id::<ApplicationMarker>::new(config::CONFIG.bot_id))
+								.create_followup(
+									&msgcmp.token,
+								)
+								.content(&message).unwrap()
+								.flags(MessageFlags::EPHEMERAL)
+								.exec()
+								.await.unwrap();
+						}
+					}
+				},
+				_ => {}
+			}
+		}
         Event::MessageCreate(message) => {
             if config::CONFIG.channels.contains(&message.channel_id.get()) {
                 if let Some(member) = &message.member {
@@ -111,16 +269,15 @@ async fn handle_event(
                 let mut message_content = message.content.split("```").peekable();
                 if message_content.peek().is_some() && message_content.next().unwrap().is_empty() {
                     if let Some(content) = message_content.next() {
-                        let mut splitted_newlines = content.split("\n").peekable();
+                        let mut splitted_newlines = content.split('\n').peekable();
                         // If the message code black isn't `rs` or `rust` then just ignore it.
                         if splitted_newlines.peek().is_some()
-                            && splitted_newlines.next().unwrap() == "rs"
+                            && splitted_newlines.next().unwrap()  == "rs"
                             || splitted_newlines.next().unwrap() == "rust"
                         {
                             http.create_reaction(message.channel_id, message.id, &loading)
                                 .exec()
-                                .await
-                                .unwrap();
+                                .await?;
 
                             let playground = play::Playground {
                                 channel: "stable".to_string(),
@@ -144,29 +301,26 @@ async fn handle_event(
                                 .method("POST")
                                 .header("User-Agent", "RunMyRust/1.0")
                                 .header("Content-Type", "application/json")
-                                .body(Body::from(serde_json::to_vec(&playground).unwrap()))
-                                .unwrap();
+                                .body(Body::from(serde_json::to_vec(&playground)?))?;
 
                             let response = match http2.request(request).await {
                                 Ok(r) => r,
                                 Err(_) => {
                                     http.create_reaction(message.channel_id, message.id, &failed)
                                         .exec()
-                                        .await
-                                        .unwrap();
+                                        .await?;
                                     return Ok(());
                                 }
                             };
 
-                            let body = hyper::body::aggregate(response).await.unwrap();
+                            let body = hyper::body::aggregate(response).await?;
                             let response: play::PlaygroundResult =
-                                serde_json::from_reader(body.reader()).unwrap();
+                                serde_json::from_reader(body.reader())?;
                             println!("{:#?}", response);
                             // if response.success == false {
                             http.create_reaction(message.channel_id, message.id, &success)
                                 .exec()
-                                .await
-                                .unwrap();
+                                .await?;
 
                             let new_thread_channel = http
                                 .create_thread_from_message(
@@ -174,27 +328,23 @@ async fn handle_event(
                                     message.id,
                                     &format!(
                                         "{}-{}",
-                                        message.author.id.to_string(),
-                                        message.id.to_string()
+                                        message.author.id,
+                                        message.id
                                     ),
-                                )
-                                .unwrap()
+                                )?
                                 .exec()
-                                .await
-                                .unwrap()
+                                .await?
                                 .model()
-                                .await
-                                .unwrap();
+                                .await?;
 
                             let message = http
-                                .create_message(new_thread_channel.id())
+                                .create_message(new_thread_channel.id)
                                 .content(&format!(
                                     "Result: {}\nStds:\n**Out:** {}\n**Err:** ```{}```",
                                     response.success,
                                     response.stdout,
-                                    response.stderr.replace("`", "\"").to_string()
-                                ))
-                                .unwrap()
+                                    response.stderr.replace('`', "\"")
+                                ))?
                                 .exec()
                                 .await;
 
@@ -211,8 +361,7 @@ async fn handle_event(
 
                         http.create_reaction(message.channel_id, message.id, &failed)
                             .exec()
-                            .await
-                            .unwrap();
+                            .await?;
                     }
                 }
             }
